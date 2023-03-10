200379d06310903dc7f5ffdb120438fc81665ef5
Home <murgu.adrian95@gmail.com>
2023-03-10 11:29:47 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/movie_tema_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movie_tema_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: MoviePage(),
+    );
+  }
+}
+
+class MoviePage extends StatefulWidget {
+  const MoviePage({Key? key}) : super(key: key);
+
+  @override
+  State<MoviePage> createState() => _MoviePageState();
+}
+
+class _MoviePageState extends State<MoviePage> {
+  @override
+  void initState() {
+    super.initState();
+    _getMovie();
+  }
+
+  final PageController controller = PageController(initialPage: 1);
+
+  bool isLoading = true;
+  bool x = true;
+
+  final List<String> _title = <String>[];
+  final List<String> _img = <String>[];
+  final List<int> _years = <int>[];
+
+  void _getMovie() {
+    get(Uri.parse('https://yts.mx/api/v2/list_movies.json')).then((Response response) {
+      response.body;
+
+      final Map<String, dynamic> map = jsonDecode(response.body) as Map<String, dynamic>;
+      final Map<String, dynamic> data = map['data'] as Map<String, dynamic>;
+      final List<dynamic> movies = data['movies'] as List<dynamic>;
+
+      setState(() {
+        _title.addAll(movies.map((dynamic item) => item['title'] as String));
+        _img.addAll(movies.map((dynamic item) => item["medium_cover_image"] as String));
+        _years.addAll(movies.map((dynamic item) => item['year'] as int));
+
+        isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Builder(
+        builder: (BuildContext context) {
+
+          if (isLoading) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return PageView.builder(
+
+              controller: controller,
+              scrollDirection: Axis.horizontal,
+              itemCount: _title.length,
+              itemBuilder: (BuildContext, int index) {
+                final String title = _title[index];
+                final String img = _img[index];
+                final int years = _years[index];
+
+                return Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                    children: [
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(child: Text('Now Showing'), onPressed: () {}),
+                          TextButton(child: Text('Cinema'), onPressed: () {}),
+                          TextButton(child: Text('Comming Soon'), onPressed: () {}),
+                        ],
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 1.2,
+                        height: MediaQuery.of(context).size.height / 1.7,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(image: NetworkImage(img), fit: BoxFit.cover),
+                        ),
+                        alignment: Alignment.center,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Container(
+                            child: Column(
+                              children: [
+                                ElevatedButton(
+                                    child: Text(
+                                      title,
+                                      style: TextStyle(
+                                          color: Colors.tealAccent, fontSize: 25, fontStyle: FontStyle.italic),
+                                    ),
+                                    onPressed: () {}),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    ElevatedButton(
+                                        child: Column(
+                                          children: [
+                                            Text('Anul '),
+                                            Text('$years'),
+                                          ],
+                                        ),
+                                        onPressed: () {}),
+                                    ElevatedButton(
+                                        child: Column(
+                                          children: [
+                                            Text('Rating'),
+                                            Text('4444'),
+                                          ],
+                                        ),
+                                        onPressed: () {}),
+                                    ElevatedButton(
+                                        child: Column(
+                                          children: [
+                                            Text('Anul '),
+                                            Text('$years'),
+                                          ],
+                                        ),
+                                        onPressed: () {}),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
